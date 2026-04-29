831db9b4a30f490d08afdd9952f4c707e1ad2c81
Ditto <dittodoangyak@gmail.com>
2026-04-29 09:51:51 +0700

    Initial Commit

---
+++ Tiktok Clone/Tiktok_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tiktok_CloneApp.swift
+//  Tiktok Clone
+//
+//  Created by Ryandra Anditto on 29/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Tiktok_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
