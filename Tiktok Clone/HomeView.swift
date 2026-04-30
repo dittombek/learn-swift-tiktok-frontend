56e41bf4971ee87d58e50ba6beb208b68b5e39e0
Ditto <dittodoangyak@gmail.com>
2026-04-30 16:44:51 +0700

    feat: add HomeView with ZStack layout and gitignore

---
+++ Tiktok Clone/HomeView.swift
@@ -0,0 +1,29 @@
+//
+//  HomeView.swift
+//  Tiktok Clone
+//
+//  Created by Ryandra Anditto on 30/04/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            // video player
+            
+            HStack {
+                Image(systemName: "livephoto")
+                    .font(.system(size: 28))
+                
+                HStack {
+                    Text("")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
